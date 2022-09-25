9445d7337b8ff2b30e434d3a51e91a252871e3b6
LucasAbucaterFiorido <lucas.abucater@hotmail.com>
2022-09-25 16:56:27 -0300

    feat: finish teste 3

--- Teste 3/index.php
+++ Teste 3/index.php
@@ -9,24 +9,97 @@
 <body style="background-color: #121212; color:aliceblue;">
     <fieldset style='width: 310px;text-align: center; margin: auto;'>
 		<br>
-		<label for="txtmenor"></label> <!-- O menor valor de faturamento ocorrido em um dia do mês -->
-		<label for="txtmaior"></label> <!-- O maior valor de faturamento ocorrido em um dia do mês -->
-		<label for="txtsuperiorMedia"></label> <!-- Número de dias no mês em que o valor de faturamento diário foi superior à média mensal -->
+		<label for="" id="txtmenor"></label> <!-- O menor valor de faturamento ocorrido em um dia do mês -->
+		<label for="" id="txtmaior"></label> <!-- O maior valor de faturamento ocorrido em um dia do mês -->
+		<label for="" id="txtsuperiorMedia"></label> <!-- Número de dias no mês em que o valor de faturamento diário foi superior à média mensal -->
 		<br><br>
     </fieldset>
 
 	<script src="../js/jquery-3.6.1.js"></script>
 	<script>
-		//const arquivo = require("faturamentoMensal.json");
-		//console.log(arquivo);
+		//var requestURL = 'http://localhost/projetos/php/GitHub/TesteGupy/teste%203/json/faturamentoMensal.json';
+		var request = new XMLHttpRequest();
+		request.open('GET', 'http://localhost/projetos/php/GitHub/TesteGupy/teste%203/json/faturamentoMensal.json'); //requestURL
+		request.responseType = 'json';
+		request.send();
+		request.onload = function()
+		{
+			var arquivo = request.response;
 
-		//const jsonData= require_once('faturamentoMensal.json'); 
-		//console.log(jsonData);
+			//var teste = JSON.parse(arquivo);
+			//var teste2 = JSON.html(arquivo)
+			var teste = arquivo.faturamentos[0].faturamento[29].status
+			var fatMes = arquivo.faturamentos[0];
 
-		// const res = await fetch("file.json");
-		// const data = await res.json();
-		// console.log(data);
+			//const dadosJSON = JSON.stringify(fatMes);
+			//console.log(typeof fatMes);
+			//console.log(fatMes.faturamento.length);
 
+			var menor = $('#txtmenor');
+			var maior = $('#txtmaior');
+			var supMedia = $('#txtsuperiorMedia');
+
+			var i = 0;
+			var diaSupMedia = 0;
+
+			do {
+				console.log();
+			} while (i < fatMes.faturamento.length);
+
+
+
+
+
+
+
+
+
+
+
+			// var i = 0;
+			// var valorMaior = 0;
+			// var valorMenor = 1000;
+			// var media = 0;
+			// var auxiliar = 0;
+			// var fds = 0;
+
+			// do 
+			// {
+			// 	valorDia = fatMes.faturamento[i].valor;
+			// 	statusDia = fatMes.faturamento[i].status;
+
+			// 	if(statusDia == "fim de semana")
+			// 	{
+			// 		fds ++;
+			// 	}
+			// 	else if(statusDia == "dia util")
+			// 	{
+			// 		if(valorMaior < valorDia)
+			// 		{
+			// 			valorMaior = valorDia;
+			// 		}
+			// 		else if(valorMenor > valorDia)
+			// 		{
+			// 			valorMenor = valorDia;
+			// 		}
+			// 	}
+				
+				//console.log(fatMes.faturamento[i]);
+
+			// 	auxiliar = auxiliar + valorDia;
+
+			// 	i++;
+			// } while (i < fatMes.faturamento.length);
+			
+			// media= auxiliar/(fatMes.faturamento.length - fds);
+			// console.log(fatMes.faturamento.length);
+			// console.log(auxiliar);
+			// console.log(media);
+
+			// menor.html(valorMenor);
+			// maior.html(valorMaior);
+			// supMedia.html(media);
+		};
 	</script>
 </body>
 </html>
